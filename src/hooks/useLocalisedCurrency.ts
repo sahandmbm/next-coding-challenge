6c95aa520c0afaa71c40fbb6645b6093262e31ad
Sahand Shahriari <sahand.shahriari@ocado.com>
2026-03-13 15:01:43 +0000

    TASK-C-5-c: add hook to get formatted currency

---
+++ src/hooks/useLocalisedCurrency.ts
@@ -0,0 +1,13 @@
+'use client';
+import { useIntl } from 'react-intl';
+import { LOCALE_CURRENCY_MAP, type Locale } from '@/lib/i18n/config';
+
+export function useLocalisedCurrency() {
+  const intl = useIntl();
+  const currency = LOCALE_CURRENCY_MAP[intl.locale as Locale] ?? LOCALE_CURRENCY_MAP['en-GB'];
+
+  const formatCurrency = (amount: number) =>
+    intl.formatNumber(amount, { style: 'currency', currency });
+
+  return { currency, formatCurrency };
+}
